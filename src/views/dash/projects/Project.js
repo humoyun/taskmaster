660db3f2e662e67e6e139cce43cde3faa38e408e
Khumoyun Akhmedov <humoyun@3i.ai>
2020-02-20 11:29:35 +0900

    new icons added

--- src/views/dash/projects/Project.js
+++ src/views/dash/projects/Project.js
@@ -1,70 +1,98 @@
 import React from "react";
-import { Skeleton, Switch, Button, Card, Icon, Avatar, Tag, Progress } from 'antd';
-import { Row, Col } from 'antd';
-import ChatNoDots from '@/icons/chat-no-dots.svg';
-import Layers from '@/icons/layers.svg';
+import {
+  Skeleton,
+  Switch,
+  Button,
+  Card,
+  Icon,
+  Avatar,
+  Tag,
+  Progress
+} from "antd";
+import { Row, Col } from "antd";
+import ChatNoDots from "@/icons/chat-no-dots.svg";
+import Layers from "@/icons/layers.svg";
+import Group from "@/icons/group.svg";
+import Attachment from "@/icons/attachment.svg";
+import More from "@/icons/more.svg";
 
 const { Meta } = Card;
 
 export default function Project(props) {
   return (
     <Card style={{ width: 360, borderRadius: 5 }} hoverable>
-    <Row>
+      <Row>
+        <Col span={24} style={{ height: "40px" }}>
+          <Row type="flex" justify="space-between">
+            <Col span={6}>
+              <Button size="small"> Go to Project</Button>
+            </Col>
+            <Col span={4}>
+              <Tag color={props.item.color}>{props.item.state}</Tag>
+            </Col>
+            <Col span={2}>
+              <Icon
+                style={{ fontSize: "20px", color: "#333" }}
+                component={More}
+              />
+            </Col>
+          </Row>
+        </Col>
 
-      <Col span={24} style={{ height: '40px' }}>
-        <Row type="flex" justify="space-between">
-          <Col span={6}>
-            <Button size='small'> Go to Project</Button>
-          </Col>
-          <Col span={4}>
-            <Tag color={props.item.color}>{props.item.state}</Tag>
-          </Col>
-          <Col span={2}>
-            <Icon style={{ fontSize: '20px', color: '#333' }} type="dash" />
-          </Col>
-        </Row>            
-      </Col>
+        <Col span={24}>
+          <h3>{props.item.title}</h3>
+          <p>{props.item.desc}</p>
+        </Col>
 
-      <Col span={24}>
-        <h3>{props.item.title}</h3>
-        <p>{props.item.desc}</p>
-      </Col>
+        <Col span={24}>
+          <Row>
+            <Col span={20} style={{ color: "#c7c7c7", fontSize: "0.9em" }}>
+              <span style={{ display: "inline-block" }} className="date-time">
+                Start date: June 8, 2019
+              </span>
+              <span style={{ display: "inline-block" }} className="date-time">
+                Due date: February 2, 2020
+              </span>
+            </Col>
+            <Col span={4}>
+              <Progress type="circle" percent={30} width={50} />
+            </Col>
+          </Row>
+        </Col>
 
-      <Col span={24}>
-        <Row>
-          <Col span={20} style={{ color: '#c7c7c7', fontSize: '0.9em' }} >                
-            <span style={{ display: 'inline-block' }} className="date-time">Start date: June 8, 2019</span>
-            <span style={{ display: 'inline-block' }} className="date-time">Due date: February 2, 2020</span>
-          </Col>
-          <Col span={4}>
-            <Progress type="circle" percent={30} width={50} />
-          </Col>
-        </Row>
-      </Col>
-
-      <Col span={18}>
-        <Row type="flex" justify="space-between">
-          <Col span={6} style={{ display: 'flex', alignContent: "center" }}>
-            <Icon style={{ fontSize: '22px', color: '#333' }} type="usergroup-add" />
-            <span style={{ marginLeft: '4px' }}>12</span>
-          </Col>
-          <Col span={6} style={{ display: 'flex', alignContent: "center" }}>
-            <Icon style={{ fontSize: '22px', color: '#333' }} component={ChatNoDots} />
-            <span style={{ marginLeft: '4px' }}>9</span>
-          </Col>
-          <Col span={6} style={{ display: 'flex', alignContent: "center" }}>
-            <Icon style={{ fontSize: '22px', color: '#333' }} type="paper-clip" />
-            <span style={{ marginLeft: '4px' }}>5</span>
-          </Col>
-          <Col span={6} style={{ display: 'flex', alignContent: "center" }}>
-            <Icon style={{ fontSize: '22px', color: '#333' }} component={Layers} />
-            <span style={{ marginLeft: '4px' }}>7/2</span>
-          </Col>
-        </Row>
-      </Col>
-
-    </Row>
-  </Card>
-
-  )
+        <Col span={18}>
+          <Row type="flex" justify="space-between">
+            <Col span={6} style={{ display: "flex", alignContent: "center" }}>
+              <Icon
+                style={{ fontSize: "22px", color: "#333" }}
+                component={Group}
+              />
+              <span style={{ marginLeft: "4px" }}>12</span>
+            </Col>
+            <Col span={6} style={{ display: "flex", alignContent: "center" }}>
+              <Icon
+                style={{ fontSize: "22px", color: "#333" }}
+                component={ChatNoDots}
+              />
+              <span style={{ marginLeft: "4px" }}>9</span>
+            </Col>
+            <Col span={6} style={{ display: "flex", alignContent: "center" }}>
+              <Icon
+                style={{ fontSize: "22px", color: "#333" }}
+                component={Attachment}
+              />
+              <span style={{ marginLeft: "4px" }}>5</span>
+            </Col>
+            <Col span={6} style={{ display: "flex", alignContent: "center" }}>
+              <Icon
+                style={{ fontSize: "22px", color: "#333" }}
+                component={Layers}
+              />
+              <span style={{ marginLeft: "4px" }}>7/2</span>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Card>
+  );
 }
