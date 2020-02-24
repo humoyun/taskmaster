79107f73bc6d56b020386eed69feeb10d965f9cb
Khumoyun Akhmedov <humoyun@3i.ai>
2020-02-24 15:21:08 +0900

    copy-webpack-plugin added

--- webpack.prod.conf.js
+++ webpack.prod.conf.js
@@ -2,18 +2,25 @@ const webpack = require("webpack");
 const path = require("path");
 const fs = require("fs");
 
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const merge = require("webpack-merge");
 const baseWebpackConf = require("./webpack.base.conf");
 const Dotenv = require("dotenv-webpack");
 
-// style files regexes
-const cssRegex = /\.css$/i;
-const cssModuleRegex = /\.module\.css$/;
-const lessRegex = /\.less$/;
-const lessModuleRegex = /\.module\.less$/;
-
 // App directory
 const appDirectory = fs.realpathSync(process.cwd());
+
+const paths = {
+  appPublic: "public",
+  appBuild: "build"
+};
+
+function copyPublicFolder() {
+  fs.copySync(paths.appPublic, paths.appBuild, {
+    dereference: true,
+    filter: file => file !== paths.appHtml
+  });
+}
 
 // Gets absolute path of file within app directory
 const resolveAppPath = relativePath => path.resolve(appDirectory, relativePath);
@@ -36,7 +43,14 @@ const prodConf = {
 
   devServer: {
     contentBase: "./build"
-  }
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: "public/assets", to: "assets" },
+      { from: "public", to: "", ignore: ["index.html", "assets/**"] }
+    ])
+  ]
 
   // plugins: [
   //   new Dotenv({
@@ -46,3 +60,13 @@ const prodConf = {
 };
 
 module.exports = merge(baseWebpackConf, prodConf);
+
+// build = {
+//   static: {
+//     js: [],
+//     css: [],
+//     assets: []
+//   },
+//   _redirects: "",
+//   favicon: ""
+// };
